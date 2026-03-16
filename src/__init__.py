c463bcc651f76e557e2a527ec3a21982b457b340
BadWolf365 <hillbillyjaka@gmail.com>
2026-03-16 16:52:20 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,9 @@
+"""
+Kerr-Descent-Engine: Core package initialization.
+"""
+
+from .engine import KerrDescentEngine
+from .descent import descent_step
+from .metrics import compute_metrics
+from .geometry import kerr_metric
 
